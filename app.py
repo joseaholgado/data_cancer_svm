35aa0b235ec77abba2ea0cd65df0494f52174d1d
Jose Antonio <115182985+joseaholgado@users.noreply.github.com>
2024-12-29 21:10:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,11 +18,21 @@ st.write("""
 
 # Crear entradas dinámicas para las características
 st.header("Introduce las características")
-feature_labels = [f"Característica {i+1}" for i in range(29)]  # Generar etiquetas
+
+# Nombres de las columnas
+feature_names = [
+    'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean',
+    'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'perimeter_se',
+    'area_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se',
+    'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst',
+    'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave points_worst', 'symmetry_worst',
+    'fractal_dimension_worst'
+]
+
 input_values = []
 
-for label in feature_labels:
-    value = st.number_input(label, value=0.0, format="%.4f")
+for feature in feature_names:
+    value = st.number_input(f"{feature.replace('_', ' ').capitalize()}", value=0.0, format="%.4f")
     input_values.append(value)
 
 # Botón para realizar la predicción
